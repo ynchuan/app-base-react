4d9b94a4ff7eefea407acaaee69741994facccaf
wxx745926676 <wangxunxun3094@qq.com>
2016-03-23 22:16:19 +0800

    webpack note

--- README.md
+++ README.md
@@ -24,8 +24,40 @@ es6除非使用webpack类似工具进行打包，转化其中的require和export
 > babel-preset-react
 > 配置文件：.babelrc<{presets:["es2015","react"]};
 > 以上工具插件可以实现es6语法的转换以及react语法转化查看
-> $ babel script.es6 -o script-compile.js  完成es6件的转化为es5 (可添加参数：--presets react --presets es2015)
+> $ babel script.es6 -o script-compile.js  完成es6件的转化为es5
 > 或者babel test-es6-to-es5 -d test-dist
+> 单独编译jsx语法以及es5的方法：采用babel+babel-preset-react插件可以进行jsx语法编译成es5文件，命令：babel example.js -o a.js --presets babel-preset-react
+> 最后要完成es6+jsx语法向es5的编译，则要配合babel-presets-es2015+babel-presets-react插件共同进行，命令中添加参数：--presets react --presets es2015
 
 采用babel配合babel插件可以实现es6和jsx语法转化为es5,但是为nodejs的api的es5语法,其最后通过webpack打包实现browser端api语法。
 以上顶层都可以通过webpack插件实现一次集成。
+
+
+webpack 使用记录
+1、命令行直接使用：
+$ webpack app.js app.bundle.js
+$ webpack app.js app.bundle.js --module-bind 'css=style!css';
+//该处或者在文件中直接添加require("!style!css!./style.css"),避免命令行再次添加。
+2、通过配置文件使用：
+$ webpack --config webpack.flux.js
+配置文件：webpack.flux.js,例如：
+module.exports={
+	entry:"app.js",
+	output:{
+		path:"built",
+		filename:"app.bundle.js"
+	},
+	module:{
+		loaders:[{
+			test:/*.css$/,
+			loader:"style!css"
+		}]
+	}
+}
+3、文件监视，自动编译：$ webpack --config webpack.flux.js --color --watch
+
+4、webpack开发服务器：
+npm install webpack-dev-server -g
+webpack-dev-server --config webpack.flux.js -w --color;
+监视文件修改，热加载文件，文件存在内存
+
--- webpack.flux.js
+++ webpack.flux.js
@@ -1,78 +1,88 @@
-var path = require('path');
-var webpack = require('webpack');
+// var path = require('path');
+// var webpack = require('webpack');
 
-module.exports = {
-    devtool: 'inline-source-map',
-    debug: true,
-    context: path.resolve(__dirname),
-    entry: [
-        'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
-        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-        './src/index.js',
-    ],
-    output: {
+// module.exports = {
+//     devtool: 'inline-source-map',
+//     debug: true,
+//     context: path.resolve(__dirname),
+//     entry: [
+//         'webpack-dev-server/client?http://0.0.0.0:8080', // WebpackDevServer host and port
+//         'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+//         './src/index.js',
+//     ],
+//     output: {
 
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: 'app.min.js',
-    },
-    resolve: {
-        root: path.resolve(__dirname),
-        extensions: ['', '.js', '.jsx'],
-        modulesDirectories: ['node_modules'],
-    },
-    node: {
-        fs: 'empty',
-    },
-    eslint: {
-        configFile: path.join(__dirname, '.eslintrc'),
-    },
-    module: {
-        preLoaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'eslint'
-        }, ],
-        loaders: [{
-            test: /\.css$/,
-            loaders: ['style', 'css'],
-        }, {
-            test: /\.json$/,
-            loader: 'json',
-        }, {
-            test: /\.less$/,
-            loaders: ['style', 'css', 'less'],
-        }, {
-            test: /\.scss$/,
-            loaders: ['style', 'css', 'sass'],
-        }, {
-            test: /\.styl$/,
-            loaders: ['style', 'css', 'stylus'],
-        }, {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loaders: ['react-hot', 'babel'],
-        }, {
-            test: /\.woff\d?(\?.+)?$/,
-            loader: 'url?limit=10000&minetype=application/font-woff',
-        }, {
-            test: /\.ttf(\?.+)?$/,
-            loader: 'url?limit=10000&minetype=application/octet-stream',
-        }, {
-            test: /\.eot(\?.+)?$/,
-            loader: 'url?limit=10000',
-        }, {
-            test: /\.svg(\?.+)?$/,
-            loader: 'url?limit=10000&minetype=image/svg+xml',
-        }, {
-            test: /\.png$/,
-            loader: 'url?limit=10000&mimetype=image/png',
-        }, {
-            test: /\.gif$/,
-            loader: 'url?limit=10000&mimetype=image/gif'
-        }],
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-    ],
-};
+//         path: path.resolve(__dirname, 'dist'),
+//         publicPath: '/',
+//         filename: 'app.min.js',
+//     },
+//     resolve: {
+//         root: path.resolve(__dirname),
+//         extensions: ['', '.js', '.jsx'],
+//         modulesDirectories: ['node_modules'],
+//     },
+//     node: {
+//         fs: 'empty',
+//     },
+//     eslint: {
+//         configFile: path.join(__dirname, '.eslintrc'),
+//     },
+//     module: {
+//         preLoaders: [{
+//             test: /\.jsx?$/,
+//             exclude: /node_modules/,
+//             loader: 'eslint'
+//         }, ],
+//         loaders: [{
+//             test: /\.css$/,
+//             loaders: ['style', 'css'],
+//         }, {
+//             test: /\.json$/,
+//             loader: 'json',
+//         }, {
+//             test: /\.less$/,
+//             loaders: ['style', 'css', 'less'],
+//         }, {
+//             test: /\.scss$/,
+//             loaders: ['style', 'css', 'sass'],
+//         }, {
+//             test: /\.styl$/,
+//             loaders: ['style', 'css', 'stylus'],
+//         }, {
+//             test: /\.jsx?$/,
+//             exclude: /node_modules/,
+//             loaders: ['react-hot', 'babel'],
+//         }, {
+//             test: /\.woff\d?(\?.+)?$/,
+//             loader: 'url?limit=10000&minetype=application/font-woff',
+//         }, {
+//             test: /\.ttf(\?.+)?$/,
+//             loader: 'url?limit=10000&minetype=application/octet-stream',
+//         }, {
+//             test: /\.eot(\?.+)?$/,
+//             loader: 'url?limit=10000',
+//         }, {
+//             test: /\.svg(\?.+)?$/,
+//             loader: 'url?limit=10000&minetype=image/svg+xml',
+//         }, {
+//             test: /\.png$/,
+//             loader: 'url?limit=10000&mimetype=image/png',
+//         }, {
+//             test: /\.gif$/,
+//             loader: 'url?limit=10000&mimetype=image/gif'
+//         }],
+//     },
+//     plugins: [
+//         new webpack.HotModuleReplacementPlugin(),
+//     ],
+// };
+
+ var path=require("path");
+ module.exports={
+    entry:"./flux-todos/app.js",
+    output:{
+        path:"./flux-todos",
+        filename:"app.bundle.js"
+    }
+
+ }
